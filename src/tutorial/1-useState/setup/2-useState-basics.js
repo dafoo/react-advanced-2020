fe285ae15b7b1c59229a1bc2736dc72e97333a42
Foo Lim <foolim@gmail.com>
2020-12-26 15:40:08 -0800

    useState hooks

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // const val = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(val, handler);
+  const [text, setText] = useState("random title 2");
+  const handleClick = () => {
+    if (text === 'random title 2') {
+      setText('hello world');
+    } else {
+      setText('random title 2');
+    }
+  }
+
+  return (<React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>
+      change title
+    </button>
+  </React.Fragment>);
 };
 
 export default UseStateBasics;
