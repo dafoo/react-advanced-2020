38a298360df8ad12f4dae151863d05d0bbeba2c3
Foo Lim <foolim@gmail.com>
2020-12-26 15:57:10 -0800

    useState with array as data value

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,24 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  return <>
+  {
+    people.map((person => {
+      const {id, name} = person;
+      return (
+        <div key={id} className='item'>
+          <h4>{name}</h4>
+        </div>
+      )
+      // console.log(person);
+      // return 'hello';
+    }))
+  }
+  <button className='btn' onClick={() => setPeople([])}>
+    clear items
+  </button>
+  </>;
 };
 
 export default UseStateArray;
