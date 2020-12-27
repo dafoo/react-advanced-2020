e237aae772490232d0b1bc8617b0ce32cfd0f57c
Foo Lim <foolim@gmail.com>
2020-12-27 12:04:37 -0800

    multiple cases when loading after fetching data from Net

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
 
-  if (loading) {
-    return <h2>Loading...</h2>  // this can be the whole app
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => console.log(user))
+      .catch(error => console.log(error));
+  }, [])
+
+  if (isLoading) {
+    return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+    )  // this can be the whole app
   }
 
-  return <h2>multiple returns</h2>;  // this can be signup page
+  if (isError) {
+    return (
+    <div>
+      <h1>Error...</h1>
+    </div>
+    )
+  }
+
+  return (
+  <div>
+    <h1>{user}</h1>
+  </div>
+  )
 };
 
 export default MultipleReturns;
