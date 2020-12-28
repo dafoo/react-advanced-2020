48e0b99b78ab2173a480d8d08d2e261a853e6ef4
Foo Lim <foolim@gmail.com>
2020-12-27 16:59:09 -0800

    using ternary operator or short circuit expression to return valid React value in component

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,14 +5,22 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
   // const [text, setText] = useState('');
   const [text, setText] = useState('foo');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return <>
     {/* <h1>{firstValue}</h1>
     <h1>value: {secondValue}</h1> */}
     <h1>{text || 'john'}</h1>
-    {text && <h1>hello world</h1>}
+    <button className='btn' onClick={() => setIsError(!isError)}>
+      toggle error
+    </button>
+    {isError && <h1>Error...</h1>}
+    {isError ? <p>there was an error...</p> :
+      <div>
+        <h2>there is no error</h2>
+      </div>}
   </>;
 };
 
