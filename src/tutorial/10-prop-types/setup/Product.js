668c1636016dc41e7e0804552e191b3f8c489990
Foo Lim <foolim@gmail.com>
2020-12-29 20:12:12 -0800

    propTypes - Default values, import local image

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';  // requires extension
 
 const Product = ({image, name, price}) => {
   return <article className='product'>
@@ -13,6 +14,13 @@ Product.propTypes = {
   image:PropTypes.object.isRequired,  // scream if not available
   name:PropTypes.string.isRequired,
   price:PropTypes.number.isRequired,
-}
+};
+
+// fallback values
+Product.defaultProps = {
+  name: 'default name',
+  price: 3.99,
+  image: defaultImage, 
+};
 
 export default Product;
