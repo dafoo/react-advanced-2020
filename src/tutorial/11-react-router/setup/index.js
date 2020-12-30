b77de6bbb55e9513242960fdf86f2cacffc3bd7a
Foo Lim <foolim@gmail.com>
2020-12-29 21:44:39 -0800

    useParams - get url params in path to dynamically show data

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -22,6 +22,8 @@ const ReactRouterSetup = () => {
       <Route path="/people">
         <People/>
       </Route>
+      <Route path="/person/:id" children={<Person/>}>
+      </Route>
       <Route path="*">
         <Error/>
       </Route>
